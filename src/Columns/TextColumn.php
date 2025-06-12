43951519092e352adf35e113eb10af7ba50a8031
Alex van Steenhoven <alex@modus-digital.com>
2025-06-12 11:39:38 +0200

    chore: changed rounded class to rounded-md

--- src/Columns/TextColumn.php
+++ src/Columns/TextColumn.php
@@ -69,7 +69,7 @@ class TextColumn extends Column
         }
 
         if ($badge) {
-            $classes = "px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-{$color}-100 text-{$color}-800 dark:bg-{$color}-800 dark:text-{$color}-100";
+            $classes = "px-2 inline-flex text-xs leading-5 font-semibold rounded-md bg-{$color}-100 text-{$color}-800 dark:bg-{$color}-800 dark:text-{$color}-100";
 
             return "<span class=\"{$classes}\">{$value}</span>";
         }
