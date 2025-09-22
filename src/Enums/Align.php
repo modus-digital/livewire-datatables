e8f0e640e6d270363a195439044d34bdaf706e4b
Alex van Steenhoven <alex@modus-digital.com>
2025-09-22 23:53:34 +0200

    feature(enums): Introduce Align enum for table alignment options
    - Added a new enum class `Align` to define alignment options: LEFT, CENTER, and RIGHT.
    - This enum will facilitate consistent alignment handling across table components.

---
+++ src/Enums/Align.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ModusDigital\LivewireDatatables\Enums;
+
+enum Align: string
+{
+    case LEFT = 'left';
+    case CENTER = 'center';
+    case RIGHT = 'right';
+}
